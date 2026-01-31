ce65f35b1c0417eadd849639490ae95e4c91e9ca
JDGB1428 <juangalindo28@hotmail.com>
2026-01-31 15:52:18 -0500

    refactor: remove unused components and layouts

--- src/app/pages/department/department-page.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-department-page',
-  imports: [],
-  templateUrl: './department-page.component.html',
-})
-export class DepartmentPageComponent { }
--- src/app/pages/offices-page/offices-page.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-offices-page',
-  imports: [],
-  templateUrl: './offices-page.component.html',
-})
-export class OfficesPageComponent { }
