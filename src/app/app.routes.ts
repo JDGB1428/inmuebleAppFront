0c36f58c8bb4b3e98fc71e937e2dabda910b01b0
JDGB1428 <juangalindo28@hotmail.com>
2026-02-03 13:08:21 -0500

    Cambios en la rutas de app routes, agregando loadchildren

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,48 +1,26 @@
-import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
 import { Routes } from '@angular/router';
-import { PrivateLayoutComponent } from './layouts/private-layout/private-layout.component';
-import { AuthLayoutComponent } from './layouts/auth-layout/auth-layout.component';
-import { NotFoundPageComponent } from './pages/not-found-page/not-found-page.component';
+
 
 export const routes: Routes = [
   {
-    path:'',
-    component:PrivateLayoutComponent,
-    children:[
-      {
-        path:'department',
-        loadComponent: () => import('./pages/department/department-page.component').then(m => m.DepartmentPageComponent)
-      },
-      {
-        path:'house',
-        loadComponent: () => import('./pages/house-page/house-page.component').then(m => m.HousePageComponent)
-      },
-      {
-        path:'land',
-        loadComponent: () => import('./pages/land-page/land-page.component').then(m => m.LandPageComponent)
-      }
-    ]
+    path:'public',
+    loadChildren: () =>import('./public-front/public.routes')
   },
   {
-    path:'admin',
-    component:AdminLayoutComponent
+    path: 'private',
+    loadChildren: () => import('./private-front/property.routes')
   },
   {
-    path:'auth',
-    component:AuthLayoutComponent,
-    children:[
-      {
-        path:'auth/login',
-        loadComponent: () => import('./pages/department/department-page.component').then(m => m.DepartmentPageComponent)
-      },
-      {
-        path:'auth/register',
-        loadComponent: () => import('./pages/house-page/house-page.component').then(m => m.HousePageComponent)
-      },
-    ]
+    path: 'auth',
+    loadChildren: () => import('./auth/auth.routes')
   },
   {
-    path:'**',
-    component:NotFoundPageComponent
+    path: 'admin',
+    loadChildren: () => import('./admin-dashboard/admin.routes')
+  },
+  {
+    path: '',
+    redirectTo:'/public/home',
+    pathMatch:'full'
   }
 ];
