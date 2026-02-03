ae6b0faadcc69497af5b91ed69ad1cfb6ef319c1
JDGB1428 <juangalindo28@hotmail.com>
2026-02-03 13:09:39 -0500

    componente de logica de admin

---
+++ src/app/admin-dashboard/layout/admin-layout/admin-layout.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-admin-layout',
+  imports: [RouterOutlet],
+  templateUrl: './admin-layout.component.html',
+})
+export class AdminLayoutComponent {
+
+}
