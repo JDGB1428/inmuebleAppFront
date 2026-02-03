24abc9363defa417eb2f1b72b6027611ab882aa5
JDGB1428 <juangalindo28@hotmail.com>
2026-02-03 14:46:29 -0500

    añadiendo rutas para la navegacion del administrador

---
+++ src/app/admin-dashboard/admin.routes.ts
@@ -0,0 +1,27 @@
+import { Routes } from "@angular/router";
+import { NotFoundPageComponent } from "../private-front/pages/not-found-page/not-found-page.component";
+import { AdminLayoutComponent } from "./layout/admin-layout/admin-layout.component";
+import { AdminPageComponent } from "./page/admin-page/admin-page.component";
+
+export const adminRoutes:Routes = [
+  {
+    path:'',
+    component: AdminLayoutComponent,
+    children:[
+      { path:'dashboard/home',
+        component: AdminPageComponent,
+      },
+      {
+        path: '**',
+        component:NotFoundPageComponent
+      },
+      {
+        path:'',
+        pathMatch:'full',
+        redirectTo: 'admin/dashboard/home'
+      }
+    ]
+  }
+];
+
+export default adminRoutes;
