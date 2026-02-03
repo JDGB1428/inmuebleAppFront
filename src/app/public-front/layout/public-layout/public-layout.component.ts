011ec68eedc42170696061c74ac0f7cea1b0150b
JDGB1428 <juangalindo28@hotmail.com>
2026-02-03 13:13:29 -0500

    creacion del componente public-layout

---
+++ src/app/public-front/layout/public-layout/public-layout.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-public-layout',
+  imports: [RouterOutlet],
+  templateUrl: './public-layout.component.html',
+})
+export class PublicLayoutComponent {
+
+}
