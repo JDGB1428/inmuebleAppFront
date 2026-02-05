3cb0c32e5c65264fef5ca1cdcc49ce18f9c7cc17
JDGB1428 <juangalindo28@hotmail.com>
2026-02-05 14:12:41 -0500

    creación de las interfaces ResponseAPi y User para la gestión de autenticación

---
+++ src/app/auth/interfaces/response_api.interfaces.ts
@@ -0,0 +1,7 @@
+import { User } from "./user.interfaces";
+
+export interface ResponseAPi {
+    token: string;
+    user:  User;
+}
+
---
+++ src/app/auth/interfaces/user.interfaces.ts
@@ -0,0 +1,8 @@
+export interface User {
+    name:       string;
+    email:      string;
+    phone:      number;
+    updated_at: Date;
+    created_at: Date;
+    id:         number;
+}
