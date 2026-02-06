e6312e2fd021d02298d676b0268094c2b269cba9
JDGB1428 <juangalindo28@hotmail.com>
2026-02-05 22:08:13 -0500

    Pasando toda la logica al form-auth

--- src/app/auth/register-page/register-page.component.ts
+++ src/app/auth/register-page/register-page.component.ts
@@ -1,39 +1,11 @@
-import { Component, computed, inject, signal } from '@angular/core';
-import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Component} from '@angular/core';
+import { FormAuthComponent } from '../component/form-auth/form-auth.component';
 
 @Component({
   selector: 'app-register',
-  imports: [ReactiveFormsModule],
+  imports: [FormAuthComponent],
   templateUrl: './register-page.component.html',
 })
 export class RegisterPageComponent {
 
-  show_password = signal(false);
-  show_confirm_password = signal(false);
-  private fb = inject(FormBuilder);
-
-  registerForm = this.fb.group({
-    name: ['', Validators.required],
-    email: ['', [Validators.required, Validators.email]],
-    password: ['', [Validators.required, Validators.minLength(6)]],
-    confirm_password: ['', [Validators.required, Validators.minLength(6)]],
-  })
-
-  conditionalSwitchPassword = computed(() => {
-    if (this.show_password() === true) {
-      return 'text';
-    }
-    return 'password';
-  });
-
-  conditionalSwitchConfirmPassword = computed(() => {
-    if (this.show_confirm_password() === true) {
-      return 'text';
-    }
-    return 'password';
-  });
-
-  onSubmit(): void {
-    console.log(this.registerForm.value);
-  }
 }
