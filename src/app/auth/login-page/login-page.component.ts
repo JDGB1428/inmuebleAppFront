26b5ad096662376202f3fb059c659dc3cbd06615
JDGB1428 <juangalindo28@hotmail.com>
2026-02-06 10:05:51 -0500

    refactor: include LoginFormComponent in login page and remove unused password handling logic

--- src/app/auth/login-page/login-page.component.ts
+++ src/app/auth/login-page/login-page.component.ts
@@ -1,29 +1,12 @@
 import { Component, computed, inject, signal } from '@angular/core';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { LoginFormComponent } from "../component/login-form/login-form.component";
 
 @Component({
   selector: 'app-login',
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, LoginFormComponent],
   templateUrl: './login-page.component.html',
 })
 export class LoginPageComponent {
-  show_password = signal(false);
-  show_confirm_password = signal(false);
-  private fb = inject(FormBuilder);
 
-  loginForm = this.fb.group({
-    email: ['', [Validators.required, Validators.email]],
-    password: ['', [Validators.required, Validators.minLength(6)]],
-  })
-
-  conditionalSwitchPassword = computed(() => {
-    if (this.show_password() === true) {
-      return 'text';
-    }
-    return 'password';
-  });
-
-  onSubmit():void {
-    console.log(this.loginForm.value);
-  }
 }
