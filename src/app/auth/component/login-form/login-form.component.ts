6cf3fbadb225c7bd3d55742bce624beb1911e599
JDGB1428 <juangalindo28@hotmail.com>
2026-02-06 10:05:35 -0500

    implementando el formulario de inicio de sesión con validaciones en el componente login-form

--- src/app/auth/component/login-form/login-form.component.ts
+++ src/app/auth/component/login-form/login-form.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject, signal } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { RouterLink } from "@angular/router";
 
 @Component({
-  selector: 'app-login-form',
-  imports: [],
+  selector: 'login-form',
+  imports: [RouterLink,ReactiveFormsModule],
   templateUrl: './login-form.component.html',
 })
 export class LoginFormComponent {
+  show_password = signal(false);
+  private fb = inject(FormBuilder);
+
+  loginForm: FormGroup = this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(8)]],
+  })
+
+  conditionalSwitchPassword = computed(() => this.show_password() ? 'text' : 'password');
+
+  onSubmit(): void {
+    if (this.loginForm.invalid) {
+      this.loginForm.markAllAsTouched(); // Para que el componente de error que creamos brille
+      return;
+    }
+    console.log(this.loginForm.value);
+  }
 
 }
