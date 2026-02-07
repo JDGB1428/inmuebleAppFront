6161219952691e40af69a67701ecb50fafa67d1b
JDGB1428 <juangalindo28@hotmail.com>
2026-02-07 18:46:24 -0500

    importando routeLink en el componente home-page

--- src/app/public-front/page/home-page/home-page.component.ts
+++ src/app/public-front/page/home-page/home-page.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home-page',
-  imports: [RouterOutlet],
+  imports: [RouterLink,],
   templateUrl: './home-page.component.html',
 })
 export class HomePageComponent {
