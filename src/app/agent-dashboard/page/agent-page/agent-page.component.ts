118a8515d73aded2269f62b117dc6f4af9861476
JDGB1428 <juangalindo28@hotmail.com>
2026-02-07 18:51:01 -0500

    componente agent creado

---
+++ src/app/agent-dashboard/page/agent-page/agent-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-agent-page',
+  imports: [],
+  templateUrl: './agent-page.component.html',
+})
+export class AgentPageComponent {
+
+}
