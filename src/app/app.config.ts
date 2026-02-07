f018c3217eaf510f86c574d98548c178c4c884c2
JDGB1428 <juangalindo28@hotmail.com>
2026-02-07 18:37:05 -0500

    implmentando interceptores para el manejo de errores

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,12 +4,13 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { authInterceptors } from './core/interceptors/auth.interceptor';
+import { errorInterceptor } from './core/interceptors/error.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
-    provideHttpClient(withInterceptors([authInterceptors])),
+    provideHttpClient(withInterceptors([authInterceptors, errorInterceptor])),
 
   ]
 };
