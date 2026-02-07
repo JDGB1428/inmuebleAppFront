ac51f0372d7e17fd09315b4009a6d9cd06ab9da8
JDGB1428 <juangalindo28@hotmail.com>
2026-02-07 17:52:42 -0500

    implementacion de role guard

---
+++ src/app/core/guards/role.guard.ts
@@ -0,0 +1,20 @@
+import { inject } from "@angular/core";
+import {CanMatchFn, Router } from "@angular/router";
+
+export const roleGuard: CanMatchFn = (route) => {
+  const router = inject(Router);
+
+  const saveRole = localStorage.getItem('role');
+  const userRoles: string[] = saveRole ? [saveRole] : [];
+
+  const expectedRoles = route.data?.['expectedRoles'] as string [];
+
+  const hasAccess = userRoles.some(role => expectedRoles.includes(role));
+
+  if (hasAccess) {
+    return true;
+  } else {
+    router.navigate(['/auth/login']);
+    return false;
+  }
+}
