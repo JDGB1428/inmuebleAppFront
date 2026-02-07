a967b4e24b21d70181a88c904bd150220deb4664
JDGB1428 <juangalindo28@hotmail.com>
2026-02-07 18:45:49 -0500

    importando componentes

--- src/app/admin-dashboard/layout/admin-layout/admin-layout.component.ts
+++ src/app/admin-dashboard/layout/admin-layout/admin-layout.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarDrawerComponent } from '../../component/navbar-drawer/navbar-drawer.component';
+import { SiderDrawerComponent } from '../../component/sider-drawer/sider-drawer.component';
 
 @Component({
   selector: 'app-admin-layout',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarDrawerComponent, SiderDrawerComponent],
   templateUrl: './admin-layout.component.html',
 })
 export class AdminLayoutComponent {
