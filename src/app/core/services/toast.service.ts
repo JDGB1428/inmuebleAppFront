787f5ff3331d06adefedde85fbdf2dd0c369c893
JDGB1428 <juangalindo28@hotmail.com>
2026-02-07 18:39:36 -0500

    servicio de toastify creado

---
+++ src/app/core/services/toast.service.ts
@@ -0,0 +1,40 @@
+import { Injectable, signal } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+
+export type ToastType = 'info' | 'success' | 'warning' | 'error';
+
+export interface ToastMessage {
+  id: number;
+  text: string;
+  type: ToastType;
+  closing: boolean;
+  duration: number;
+}
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ToastService {
+  private _toasts = signal<ToastMessage[]>([]);
+  readonly toasts = this._toasts.asReadonly();
+
+  private counter = 0;
+
+  show(text: string, type: ToastType = 'info', duration = 3000) {
+    const id = ++this.counter;
+    // Guardamos la duración en el objeto
+    this._toasts.update(c => [...c, { id, text, type, closing: false, duration }]);
+
+    if (duration > 0) setTimeout(() => this.startRemove(id), duration);
+  }
+
+  startRemove(id: number) {
+    this._toasts.update(current =>
+      current.map(t => t.id === id ? { ...t, closing: true } : t)
+    );
+
+    setTimeout(() => {
+      this._toasts.update(current => current.filter(t => t.id !== id));
+    }, 200);
+  }
+}
