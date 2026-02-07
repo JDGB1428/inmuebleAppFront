d6aaa884e9b8dd6fd96bd53f0bc21938fb83a00d
JDGB1428 <juangalindo28@hotmail.com>
2026-02-07 18:37:50 -0500

    Creando componente navbar-drawer

---
+++ src/app/admin-dashboard/component/navbar-drawer/navbar-drawer.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'navbar-drawer',
+  imports: [],
+  templateUrl: './navbar-drawer.component.html',
+})
+export class NavbarDrawerComponent {
+
+}
