be770c470a0f42ef8a2031ae48a4d4a49c6ab845
JDGB1428 <juangalindo28@hotmail.com>
2026-02-07 14:11:43 -0500

    inyectando services y creando funcion para cerrar la sesion

--- src/app/private-front/components/navbar/navbar.component.ts
+++ src/app/private-front/components/navbar/navbar.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { AuthService } from '../../../core/services/auth.service';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'navbar',
@@ -7,5 +9,12 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
   templateUrl: './navbar.component.html',
 })
 export class NavbarComponent {
+  private authService = inject(AuthService);
+  private location = inject(Location)
 
+  logout():void {
+    this.authService.authLogout().subscribe(() => {
+      this.location.back();
+    });
+  }
 }
