10c68e0d58fc760256ca25ded1f07a6079c7b566
JDGB1428 <juangalindo28@hotmail.com>
2026-02-07 18:40:50 -0500

    layout de vista del role agent creado

---
+++ src/app/agent-dashboard/layout/agent-layout/agent-layout.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-agent-layout',
+  imports: [RouterOutlet],
+  templateUrl: './agent-layout.component.html',
+})
+export class AgentLayoutComponent {
+
+}
