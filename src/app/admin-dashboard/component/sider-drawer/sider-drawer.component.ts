90b50aa88a538700cbab9d70fc8a82756e3612af
JDGB1428 <juangalindo28@hotmail.com>
2026-02-07 17:51:26 -0500

    componente sider creado

---
+++ src/app/admin-dashboard/component/sider-drawer/sider-drawer.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'sider-drawer',
+  imports: [],
+  templateUrl: './sider-drawer.component.html',
+})
+export class SiderDrawerComponent {
+
+}
