00198decb31254fa91af52ce39a5e4df0ca28fa3
JDGB1428 <juangalindo28@hotmail.com>
2026-02-08 17:18:31 -0500

    importando RouterLink

--- src/app/admin-dashboard/layout/admin-layout/admin-layout.component.ts
+++ src/app/admin-dashboard/layout/admin-layout/admin-layout.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterLinkWithHref, RouterLink } from '@angular/router';
 import { NavbarDrawerComponent } from '../../component/navbar-drawer/navbar-drawer.component';
 import { SiderDrawerComponent } from '../../component/sider-drawer/sider-drawer.component';
 
 @Component({
   selector: 'app-admin-layout',
-  imports: [RouterOutlet, NavbarDrawerComponent, SiderDrawerComponent],
+  imports: [RouterOutlet, RouterLink, NavbarDrawerComponent, SiderDrawerComponent,],
   templateUrl: './admin-layout.component.html',
 })
 export class AdminLayoutComponent {
