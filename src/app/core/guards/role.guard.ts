d59c999f2c90a9144b573de7653cfcb4f3deb851
JDGB1428 <juangalindo28@hotmail.com>
2026-02-08 17:15:53 -0500

    Agregando role de sessionStorage

--- src/app/core/guards/role.guard.ts
+++ src/app/core/guards/role.guard.ts
@@ -4,7 +4,7 @@ import {CanMatchFn, Router } from "@angular/router";
 export const roleGuard: CanMatchFn = (route) => {
   const router = inject(Router);
 
-  const saveRole = localStorage.getItem('role');
+  const saveRole = localStorage.getItem('roles') || sessionStorage.getItem('roles')
   const userRoles: string[] = saveRole ? [saveRole] : [];
 
   const expectedRoles = route.data?.['expectedRoles'] as string [];
