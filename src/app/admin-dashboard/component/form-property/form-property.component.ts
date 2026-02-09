e24376dbf30387501d91596d365e6e57cb3f32f7
JDGB1428 <juangalindo28@hotmail.com>
2026-02-09 17:12:28 -0500

    cambiando el nombre del form en el selector

--- src/app/admin-dashboard/component/form-property/form-property.component.ts
+++ src/app/admin-dashboard/component/form-property/form-property.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-form-property',
+  selector: 'form-property',
   imports: [],
   templateUrl: './form-property.component.html',
 })
