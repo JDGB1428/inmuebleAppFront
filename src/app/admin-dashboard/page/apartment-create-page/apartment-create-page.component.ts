c96539fb0257cbcf8b2cfd542e19a13019690bc6
JDGB1428 <juangalindo28@hotmail.com>
2026-02-09 17:01:01 -0500

    importando el formulario form property

--- src/app/admin-dashboard/page/apartment-create-page/apartment-create-page.component.ts
+++ src/app/admin-dashboard/page/apartment-create-page/apartment-create-page.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { FormPropertyComponent } from "../../component/form-property/form-property.component";
 
 @Component({
   selector: 'app-apartment-create-page',
-  imports: [],
+  imports: [FormPropertyComponent],
   templateUrl: './apartment-create-page.component.html',
 })
 export class ApartmentCreatePageComponent {
