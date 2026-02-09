77f10315e94e830471617feb55587590a1c3a771
JDGB1428 <juangalindo28@hotmail.com>
2026-02-09 16:19:01 -0500

    pagina para la vista formulario creada

---
+++ src/app/admin-dashboard/page/apartment-create-page/apartment-create-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-apartment-create-page',
+  imports: [],
+  templateUrl: './apartment-create-page.component.html',
+})
+export class ApartmentCreatePageComponent {
+
+}
