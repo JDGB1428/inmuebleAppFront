a0745c3872814ca94285907f1191d5d49fb9b64b
JDGB1428 <juangalindo28@hotmail.com>
2026-02-12 11:44:17 -0500

    Importando componente

--- src/app/admin-dashboard/page/apartment-admin-page/apartment-page.component.ts
+++ src/app/admin-dashboard/page/apartment-admin-page/apartment-page.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterLink } from "@angular/router";
+import { CardDepartmentComponent } from "../../../property/component/card-department/card-department.component";
 
 @Component({
   selector: 'app-apartment-page',
-  imports: [RouterLink],
+  imports: [RouterLink, CardDepartmentComponent],
   templateUrl: './apartment-page.component.html',
 })
 export class ApartmentPageComponent {
