f512d64ffa5f0d473b5adf46b1d7d875570de6fa
JDGB1428 <juangalindo28@hotmail.com>
2026-02-12 11:41:37 -0500

    creando el services de categoria y creando funcion para mostrar la categoria

---
+++ src/app/core/services/category.service.ts
@@ -0,0 +1,26 @@
+
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { environment } from '../../../environments/environment';
+import { catchError, map, Observable, of, tap} from 'rxjs';
+import { CategoryAdapter } from '../interfaces/category.interfaces';
+import { CategoryModel } from '../model/category.model';
+import { HTTPResponseCategory } from '../interfaces/response_api.interfaces';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CategoryServices {
+  private apiUrl = environment.LaravelAPI;
+  private http = inject(HttpClient);
+
+
+  getCategories(): Observable<CategoryAdapter[]>{
+    return this.http.get<HTTPResponseCategory>(`${this.apiUrl}/api/category`).pipe(
+      map((response) => {
+        return CategoryModel.mapHttpReponseCategoryToCategoryAdapterArray(response.data);
+      }),
+      catchError((error) => of([]))
+    )
+  }
+}
