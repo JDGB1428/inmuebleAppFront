6bda3ee0c90c9896d6766dbd962111d9ac191fcd
JDGB1428 <juangalindo28@hotmail.com>
2026-02-17 17:27:30 -0500

    feat: creando la funcion de mostrar todas las propiedades, modificando la funcion para crear propiedades y creando mapper para limpiar los datos que provienen del backend

--- src/app/core/services/property.service.ts
+++ src/app/core/services/property.service.ts
@@ -1,7 +1,11 @@
 import { inject, Injectable } from '@angular/core';
 import { environment } from '../../../environments/environment';
 import { HttpClient } from '@angular/common/http';
-import { catchError, tap, throwError } from 'rxjs';
+import { catchError, map, Observable, of, tap, throwError } from 'rxjs';
+import { PropertyModel } from '../model/property.model';
+import { ApiResponse, BackendResponseDto } from '../interfaces/http-reponses.interfaces';
+import { PropertyDTO } from '../interfaces/response-dto.interfaces';
+import { Property } from '../interfaces/property.interfaces';
 
 @Injectable({
   providedIn: 'root',
@@ -11,10 +15,24 @@ export class PropertyServices {
   private http = inject(HttpClient);
 
 
-  createProperty(data:FormData){
-    return this.http.post(`${this.apiUrl}/api/property`, data).pipe(
-      tap((response)=>{
-        console.log(response)
+  getAllProperty():Observable<ApiResponse<Property[]>>{
+    return this.http.get<BackendResponseDto<PropertyDTO[]>>(`${this.apiUrl}/api/property`).pipe(
+      tap((response) => {
+        console.log(response);
+      }),
+      map((response) => PropertyModel.mapToHttpResponsePropertyToListProperties(response)),
+      catchError(() => of( {
+        message : 'Error al cargar propiedades',
+        data: []
+      }))
+    )
+  }
+
+
+  createProperty(data:FormData):Observable<ApiResponse<Property>>{
+    return this.http.post<BackendResponseDto<PropertyDTO>>(`${this.apiUrl}/api/property`, data).pipe(
+      map((response) => {
+        return PropertyModel.mapToHttpResponsePropertyToPropertyArray(response)
       }),
       catchError((error) => {
         return throwError(()=> error)
