9cea90078a2944db5729eda077c1f71b3164a72c
JDGB1428 <juangalindo28@hotmail.com>
2026-03-05 10:58:42 -0500

    creacion del componente loading card

---
+++ src/app/shared/component/loading-card/loading-card.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'shared-loading-card',
+  imports: [],
+  templateUrl: './loading-card.component.html',
+})
+export class LoadingCardComponent {
+
+}
