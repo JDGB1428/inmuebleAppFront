c31c789bfb4a39b5bdc1b888fc3ded3d17df657a
JDGB1428 <juangalindo28@hotmail.com>
2026-03-05 10:59:51 -0500

    Eliminando interfaces de mas

--- src/app/core/interfaces/http-reponses.interfaces.ts
+++ src/app/core/interfaces/http-reponses.interfaces.ts
@@ -1,28 +1,16 @@
-import { CategoryDto } from "./response-dto.interfaces";
+import { CategoryDTO } from "./response-dto.interfaces";
 import { User } from "./response_api.interfaces";
 
-export interface HttpResponseLaravelAPi {
+export interface HttpResponseLaravelAPi{
     token: string;
-    user:  User;
+    data:  User;
 }
 
 export interface HttpResponseProperty<T> {
-    message: string;
+    message?: string;
     data: T;
 }
 
-export interface ApiResponse<T> {
-  message: string;
-  data: T;
-}
-
-export interface BackendResponseDto<T> {
-  message: string;
-  data?: T | T[];
-  property?: T;
-  user?: T;
-}
-
 export interface HTTPResponseCategory {
-    data: CategoryDto[];
+    data: CategoryDTO[];
 }
