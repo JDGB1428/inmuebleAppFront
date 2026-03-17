32303a64f7b7d13ecfcb68a5019686390b3005a4
JDGB1428 <juangalindo28@hotmail.com>
2026-03-17 09:20:37 -0500

    Componente de public de home

--- src/app/public-front/page/home-page/home-page.component.ts
+++ src/app/public-front/page/home-page/home-page.component.ts
@@ -1,9 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { HeroComponent } from "../../component/hero/hero.component";
+import { FeaturesComponent } from "../../component/features/features.component";
+import { HowItWorksComponent } from "../../component/how-it-works/how-it-works.component";
+import { FooterComponent } from "../../component/footer/footer.component";
+import { FaqComponent } from "../../component/faq/faq.component";
+import { Navbar } from "../../../shared/component/navbar/navbar.component";
+import { PricingComponent } from "../../component/pricing/pricing.component";
 
 @Component({
   selector: 'app-home-page',
-  imports: [RouterLink,],
+  imports: [HeroComponent, FeaturesComponent, HowItWorksComponent, FooterComponent, FaqComponent, Navbar, PricingComponent],
   templateUrl: './home-page.component.html',
 })
 export class HomePageComponent {
