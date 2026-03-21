868fa3c66b4ec24fe484bd62b87c8c90c0722e37
JDGB1428 <juangalindo28@hotmail.com>
2026-03-21 15:45:24 -0500

    inyectando el servicio de authService y validando el rol del usuario, en la vista cambio en el nombre del component del navbar, eliminando paths

--- src/app/admin-dashboard/layout/admin-layout/admin-layout.component.ts
+++ src/app/admin-dashboard/layout/admin-layout/admin-layout.component.ts
@@ -1,13 +1,17 @@
-import { Component } from '@angular/core';
-import { RouterOutlet, RouterLinkWithHref, RouterLink } from '@angular/router';
-import { NavbarDrawerComponent } from '../../component/navbar-drawer/navbar-drawer.component';
-import { SiderDrawerComponent } from '../../component/sider-drawer/sider-drawer.component';
+import { Component, inject } from '@angular/core';
+import { RouterLink, RouterOutlet} from '@angular/router';
+import { AuthService } from '../../../core/services/auth.service';
+import { Navbar } from "../../../shared/component/navbar/navbar.component";
 
 @Component({
   selector: 'app-admin-layout',
-  imports: [RouterOutlet, RouterLink, NavbarDrawerComponent, SiderDrawerComponent,],
+  imports: [RouterOutlet, RouterLink, Navbar],
   templateUrl: './admin-layout.component.html',
 })
 export class AdminLayoutComponent {
+  private authService = inject(AuthService);
+
+  isAgent = this.authService.isAgent();
+  isAdmin = this.authService.isAdmin();
 
 }
