bd4745298c843d36814a207591a89c6592814271
JDGB1428 <juangalindo28@hotmail.com>
2026-03-21 15:48:32 -0500

    implmentando  NgOnInit para el cargue de propiedades, inyectando los servicios de autenticacion para validar el rol del usuario

---
+++ src/app/admin-dashboard/page/property-admin-page/property-page.component.ts
@@ -0,0 +1,43 @@
+
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { RouterLink } from "@angular/router";
+import { CardPropertyComponent } from "../../../property/component/card-property/card-property.component";
+import { PropertyServices } from '../../../core/services/property.service';
+import { Property } from '../../../core/interfaces/property.interfaces';
+import { LoadingCardComponent } from "../../../shared/component/loading-card/loading-card.component";
+import { AuthService } from '../../../core/services/auth.service';
+
+@Component({
+  selector: 'app-apartment-page',
+  imports: [RouterLink, CardPropertyComponent, LoadingCardComponent],
+  templateUrl: './property-page.component.html',
+})
+export class ApartmentPageComponent implements OnInit {
+
+  properties = signal<Property[]>([]);
+  loading = signal<boolean>(true);
+  loadingCards = Array(8);
+
+
+  private propertyService = inject(PropertyServices);
+  private authService = inject(AuthService);
+
+  isAgent = this.authService.isAgent();
+  isAdmin = this.authService.isAdmin();
+
+   ngOnInit(): void {
+    setTimeout(() => {
+      this.listProperties();
+      this.loading.set(false);
+    }, 2000);
+  }
+
+  listProperties(){
+    this.propertyService.getAllProperty().subscribe((properties) => {
+      if(!properties) return [];
+      return this.properties.set(properties.data);
+    })
+  }
+
+
+}
