65cad44e2b15d7cdc3e1439829a002843d7d5fdd
JDGB1428 <juangalindo28@hotmail.com>
2026-03-21 13:50:50 -0500

    Component del page de profile

---
+++ src/app/admin-dashboard/page/profile-admin-page/profile-admin-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { FormProfileComponent } from "../../../profile/component/form-profile/form-profile.component";
+
+@Component({
+  selector: 'app-profile-admin-page',
+  imports: [FormProfileComponent],
+  templateUrl: './profile-admin-page.component.html',
+})
+export class ProfileAdminPageComponent {
+
+}
