b5afc287c3bd33909a62478f6d4fa85400c18196
JDGB1428 <juangalindo28@hotmail.com>
2026-03-21 14:12:41 -0500

    implementando ngOnInit para cargar los datos de los perfiles

--- src/app/admin-dashboard/page/user-admin-page/user-admin-page.component.ts
+++ src/app/admin-dashboard/page/user-admin-page/user-admin-page.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { ProfileService } from '../../../core/services/profile.service';
+import { Profile} from '../../../core/interfaces/response_api.interfaces';
 
 @Component({
   selector: 'app-user-admin-page',
   imports: [],
   templateUrl: './user-admin-page.component.html',
 })
-export class UserAdminPageComponent {
+export class UserAdminPageComponent implements OnInit {
+
+
+  private readonly profileService = inject(ProfileService);
+
+  profiles = signal<Profile[]>([]);
+
+  ngOnInit(): void {
+    this.listProfiles();
+  }
+
+  listProfiles(){
+    this.profileService.getProfiles().subscribe((profile) => {
+      this.profiles.set(profile.active);
+      console.log(profile.active);
+    })
+  }
+
 
 }
