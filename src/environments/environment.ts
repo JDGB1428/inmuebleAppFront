5c062243122dbfad7def738489265dec1cce0189
JDGB1428 <juangalindo28@hotmail.com>
2026-03-21 13:45:10 -0500

    environment de reverb para hacer la notificaciones

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,3 +1,10 @@
 export const environment = {
   LaravelAPI: 'http://localhost:8000',
+
+  reverb: {
+    key:'qyvdgve5ncsjudhimnxy',
+    host:'localhost',
+    port:8080,
+    useTLS:false
+  }
 };
