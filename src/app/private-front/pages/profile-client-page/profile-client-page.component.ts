015c0c4b80226294e495c804c810ff6e4b5e1d44
JDGB1428 <juangalindo28@hotmail.com>
2026-03-21 13:51:49 -0500

    componente del page profile por parte del ciente

---
+++ src/app/private-front/pages/profile-client-page/profile-client-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { FormProfileComponent } from "../../../profile/component/form-profile/form-profile.component";
+
+@Component({
+  selector: 'app-profile-client-page',
+  imports: [FormProfileComponent],
+  templateUrl: './profile-client-page.component.html',
+})
+export class ProfileClientPageComponent {
+
+}
