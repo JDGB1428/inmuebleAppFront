f808f3a307c72a6455dd4aeb7c8f0287b3b15353
JDGB1428 <juangalindo28@hotmail.com>
2026-03-21 13:55:06 -0500

    trayendo del service authService los roles de la usuario autenticado

--- src/app/private-front/pages/not-found-page/not-found-page.component.ts
+++ src/app/private-front/pages/not-found-page/not-found-page.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { AuthService } from '../../../core/services/auth.service';
 
 @Component({
   selector: 'app-not-found-page',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './not-found-page.component.html',
 })
 export class NotFoundPageComponent {
+  private readonly authService = inject(AuthService);
+
+  isAgent = this.authService.isAgent
+  isAdmin = this.authService.isAdmin
+  isClient = this.authService.isClient
 
 }
