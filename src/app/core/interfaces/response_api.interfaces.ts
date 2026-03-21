20a68d47e6b214ab84e9757e838b2619d37a91ba
JDGB1428 <juangalindo28@hotmail.com>
2026-03-21 14:13:41 -0500

    Creacion de las interfaces para hacer el mapper de los datos proveniente del backend

--- src/app/core/interfaces/response_api.interfaces.ts
+++ src/app/core/interfaces/response_api.interfaces.ts
@@ -1,3 +1,5 @@
+import { AuthUserResponseDTO } from "./response-dto.interfaces";
+
 export interface User {
     name:       string;
     email:      string;
@@ -12,6 +14,62 @@ export interface PropertyImage {
   id: number;
   url: string;
 }
+
+// model/profile.model.ts
+
+export interface UserTheProfile {
+  id: number;
+  name: string;
+  email: string;
+}
+
+export interface Profile {
+  id: number;
+  userId: number;
+  address: string;
+  avatar: string;
+  phone: string;
+  whatsapp: string;
+  jobTitle: string;        // Quitamos el null para simplificar el HTML
+  yearsOfExperience: number | null;
+  specialties: string | null;
+  socialLinks: string | null;
+  user: UserTheProfile;
+}
+
+export interface ProfileGroups {
+  active: Profile[];
+  trashed: Profile[];
+}
+
+export interface AuthUserApiResponse {
+  data: AuthUserResponseDTO;
+}
+
+// --- Modelos Frontend (Lo que usará tu componente) ---
+
+// Perfil adaptado a camelCase
+export interface UserProfileData {
+  id: number;
+  userId: number;
+  address: string;
+  avatar: string;
+  phone: string;
+  whatsapp: string;
+  jobTitle: string;
+  yearsOfExperience: number | null;
+  specialties: string | null;
+  socialLinks: string | null;
+}
+
+// Usuario adaptado
+export interface AuthUser {
+  id: number;
+  name: string;
+  email: string;
+  profile: UserProfileData | null;
+}
+
 
 
 
