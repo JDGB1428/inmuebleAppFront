a4bdc85660748804ea2dcd752e790ea48398dd61
JDGB1428 <juangalindo28@hotmail.com>
2026-03-21 13:56:31 -0500

    creando types DTO del perfiles provenientes del backend

--- src/app/core/interfaces/response-dto.interfaces.ts
+++ src/app/core/interfaces/response-dto.interfaces.ts
@@ -1,3 +1,4 @@
+
 export type PropertyDTO = {
   id: number;
   title: string;
@@ -14,7 +15,7 @@ export type PropertyDTO = {
   updated_at: string;
   created_at: string;
   deleted_at?: string | null;
-  feature: FeaturesDTO;
+  features: FeaturesDTO;
 }
 
 export type CategoryDTO = {
@@ -24,9 +25,73 @@ export type CategoryDTO = {
 }
 
 export type FeaturesDTO = {
-  patio: boolean;
-  terrace: boolean;
-  pool: boolean;
-  gated_community: boolean;
-  security_24_7: boolean;
+  patio?: boolean;
+  terrace?: boolean;
+  pool?: boolean;
+  gated_community?: boolean;
+  security_24_7?: boolean;
+  bbq_zone?: boolean;
+  balcony?: boolean;
+  gym?: boolean;
+  parking?: boolean;
+  administration?: boolean;
 }
+
+
+export type UserResponseDTO = {
+  id: number;
+  name: string;
+  email: string;
+}
+
+export type ProfileResponseDTO = {
+  id: number;
+  user_id: number;
+  address: string;
+  avatar: string;
+  phone: string;
+  whatsapp: string;
+  job_title: string | null;
+  years_of_experience: number | null;
+  specialties: string | null;
+  social_links: string | null;
+  created_at: string;
+  updated_at: string;
+  deleted_at: string | null;
+  user: UserResponseDTO;
+}
+
+export type ProfileGroupsResponse =  {
+  active: ProfileResponseDTO[];
+  trashed: ProfileResponseDTO[];
+}
+
+export type ApiResponse =  {
+  data: ProfileGroupsResponse;
+}
+
+export type SingleProfileResponseDTO = {
+  id: number;
+  user_id: number;
+  address: string;
+  avatar: string;
+  phone: string;
+  whatsapp: string;
+  job_title: string | null;
+  years_of_experience: number | null;
+  specialties: string | null;
+  social_links: string | null;
+  created_at?: string;
+  updated_at?: string;
+  deleted_at?: string | null;
+}
+
+export type AuthUserResponseDTO = {
+  id: number;
+  name: string;
+  email: string;
+  profile: SingleProfileResponseDTO | null; // Puede no tener perfil aún
+}
+
+
+
