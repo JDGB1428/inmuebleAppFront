3f3cae702572a5f1b255d268ca7b7bd7d08f9f46
JDGB1428 <juangalindo28@hotmail.com>
2026-03-30 17:06:46 -0500

    implementando observables y tipando la funcion

--- src/app/core/services/commentary.service.ts
+++ src/app/core/services/commentary.service.ts
@@ -1,6 +1,8 @@
 import { inject, Injectable } from '@angular/core';
 import { environment } from '../../../environments/environment';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Commentary } from '../interfaces/response_api.interfaces';
 
 @Injectable({
   providedIn: 'root',
@@ -10,12 +12,12 @@ export class CommentaryService {
   private apiUrl = environment.LaravelAPI;
   private http = inject(HttpClient);
 
-  getCommentaries(id:number, data:any){
-    return this.http.get(`${this.apiUrl}/api/property/${id}/comments`,data);
+  getCommentaries(id:number):Observable<Commentary[]>{
+    return this.http.get<Commentary[]>(`${this.apiUrl}/api/property/${id}/comments`);
   }
 
-  createCommentary(id:number, data:any){
-    return this.http.post(`${this.apiUrl}/api/property/${id}/comment`,data);
+  createCommentary(id:number, data:{description:string}):Observable<Commentary>{
+    return this.http.post<Commentary>(`${this.apiUrl}/api/property/${id}/comment`,data);
   }
 
 }
