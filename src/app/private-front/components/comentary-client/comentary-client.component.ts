7d8fc5521e12c50d4e7ddb9b3535ce1a7baddbd5
JDGB1428 <juangalindo28@hotmail.com>
2026-03-30 18:08:26 -0500

    implementando formulario reactivo para la creacion de comentarios, inyectando los services de comentarios

--- src/app/private-front/components/comentary-client/comentary-client.component.ts
+++ src/app/private-front/components/comentary-client/comentary-client.component.ts
@@ -1,22 +1,29 @@
-import { Component, inject, OnInit, signal } from '@angular/core';
+import { Component, inject, input, OnInit, signal } from '@angular/core';
 import { AuthService } from '../../../core/services/auth.service';
 import { DatePipe } from '@angular/common';
 import { ProfileService } from '../../../core/services/profile.service';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { CommentaryService } from '../../../core/services/commentary.service';
+import { Commentary } from '../../../core/interfaces/response_api.interfaces';
 
 @Component({
   selector: 'comentary-client',
-  imports: [DatePipe, ReactiveFormsModule],
+  imports: [ReactiveFormsModule, DatePipe],
   templateUrl: './comentary-client.component.html',
 })
 export class ComentaryClient implements OnInit {
 
   private readonly authService = inject(AuthService);
   private readonly profileService = inject(ProfileService);
+  private readonly fb = inject(FormBuilder);
+  private readonly commentaryService = inject(CommentaryService);
   avatar = signal<string | undefined>('');
-
-
   isClient = this.authService.isClient();
+  commentary = signal<Commentary[]>([]);
+
+  propertyId = input.required<number>();
+
+
 
   ngOnInit(): void {
     this.profileService.getProfileById().subscribe({
@@ -24,5 +31,30 @@ export class ComentaryClient implements OnInit {
         this.avatar.set(profile.profile?.avatar)
       }
     })
+
+    this.commentaryService.getCommentaries(this.propertyId()).subscribe((data) => {
+      this.commentary.set(data);
+    })
   }
+
+  commentaryForm:FormGroup  = this.fb.group({
+    'description':['', Validators.required]
+  });
+
+
+  onSubmit(){
+    if(this.commentaryForm.valid){
+      const payload = {
+        description : this.commentaryForm.value.description
+      }
+      this.commentaryService.createCommentary(this.propertyId(), payload).subscribe(() => {
+        this.commentaryForm.reset();
+      })
+    }
+
+  }
+
+
+
+
 }
