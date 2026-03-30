04f440d93f353a0f7c4deaa2026e5136cc455360
JDGB1428 <juangalindo28@hotmail.com>
2026-03-30 15:35:36 -0500

    implementando ngOnInit, inyectando services

--- src/app/private-front/components/comentary-client/comentary-client.component.ts
+++ src/app/private-front/components/comentary-client/comentary-client.component.ts
@@ -1,14 +1,28 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { AuthService } from '../../../core/services/auth.service';
 import { DatePipe } from '@angular/common';
+import { ProfileService } from '../../../core/services/profile.service';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'comentary-client',
-  imports: [DatePipe],
+  imports: [DatePipe, ReactiveFormsModule],
   templateUrl: './comentary-client.component.html',
 })
-export class ComentaryClient {
+export class ComentaryClient implements OnInit {
+
   private readonly authService = inject(AuthService);
+  private readonly profileService = inject(ProfileService);
+  avatar = signal<string | undefined>('');
+
 
   isClient = this.authService.isClient();
+
+  ngOnInit(): void {
+    this.profileService.getProfileById().subscribe({
+      next: (profile) => {
+        this.avatar.set(profile.profile?.avatar)
+      }
+    })
+  }
 }
