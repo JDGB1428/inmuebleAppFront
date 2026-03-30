18b8c9b011b136f486d7ab518fa644c37f6ee01c
JDGB1428 <juangalindo28@hotmail.com>
2026-03-30 17:09:28 -0500

    Creacion del componente para la lista de comentarios

---
+++ src/app/private-front/components/list-commentary/list-commentary.component.ts
@@ -0,0 +1,11 @@
+import { DatePipe } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-list-commentary',
+  imports: [DatePipe],
+  templateUrl: './list-commentary.component.html',
+})
+export class ListCommentaryComponent {
+
+}
