2422f66324dffd13fe015e3c461799c347a7c808
JDGB1428 <juangalindo28@hotmail.com>
2026-03-31 13:39:16 -0500

    creando la funcion goToProperty para marcar la notificacion vista, creando el viewChild,  funciones para marcar todos los leidos y mostrar las notificaciones de los no leidos terminados

--- src/app/shared/component/navbar/navbar.component.ts
+++ src/app/shared/component/navbar/navbar.component.ts
@@ -1,6 +1,6 @@
-import { Component, computed, inject, OnInit, signal } from '@angular/core';
+import { Component, computed, ElementRef, inject, OnInit, signal, viewChild } from '@angular/core';
 import { AuthService } from '../../../core/services/auth.service';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 import { Location } from '@angular/common';
 import { ProfileService } from '../../../core/services/profile.service';
 import { AuthUser, AuthUserApiResponse, Profile } from '../../../core/interfaces/response_api.interfaces';
@@ -13,6 +13,7 @@ import { NotificationService } from '../../../core/services/notification.service
 })
 
 export class Navbar implements OnInit {
+  private readonly router = inject(Router);
   private readonly authService = inject(AuthService);
   private readonly profileService = inject(ProfileService);
   private readonly notificationService = inject(NotificationService);
@@ -21,43 +22,58 @@ export class Navbar implements OnInit {
   isClient = this.authService.isClient();
   isAgent = this.authService.isAgent();
   isAdmin = this.authService.isAdmin();
-  profiles = signal<AuthUser|null>(null);
+  profiles = signal<AuthUser | null>(null);
 
   notifications = this.notificationService.notifications;
   unreadCount = computed(() => this.notifications().length);
 
-  ngOnInit(): void {
-    this.getProfile();
-    this.initNotifications();
+  dropdownElement = viewChild<ElementRef<HTMLDivElement>>('notifDropdown');
 
+  ngOnInit(): void {
+
+
+    if (this.isClient || this.isAdmin || this.isAgent) {
+      this.getProfile();
+      this.initNotifications();
+    }
   }
 
 
-  getProfile():void{
+  getProfile(): void {
     this.profileService.getProfileById().subscribe((profile) => {
       this.profiles.set(profile);
     })
   }
+
 
   initNotifications(): void {
     const user = this.authService.currentUser();
     const isClientRole = this.authService.isClient();
 
     if (user && isClientRole) {
+      this.notificationService.fetchUnreadNotificationsFromDB();
       this.notificationService.listenForProperties(user.id);
     }
   }
 
   markAsRead(): void {
-    this.notificationService.notifications.set([]);
+    this.notificationService.markAsReadInDB();
   }
 
 
 
-  logout():void {
+  logout(): void {
     this.authService.authLogout().subscribe(() => {
       this.location.back();
     });
+  }
+
+
+  goToProperty(propertyId: number): void {
+
+    this.notificationService.markPropertyAsRead(propertyId);
+    this.router.navigate(['/private/home/show', propertyId]);
+    this.dropdownElement()?.nativeElement.blur();
   }
 
 }
