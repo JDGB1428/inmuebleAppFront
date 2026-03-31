ec4d3e4a77b9b529c72e81cf784a198665030374
JDGB1428 <juangalindo28@hotmail.com>
2026-03-31 15:59:44 -0500

    importando data-table

--- src/app/admin-dashboard/page/user-admin-page/user-admin-page.component.ts
+++ src/app/admin-dashboard/page/user-admin-page/user-admin-page.component.ts
@@ -1,10 +1,12 @@
 import { Component, inject, OnInit, signal } from '@angular/core';
 import { ProfileService } from '../../../core/services/profile.service';
 import { Profile} from '../../../core/interfaces/response_api.interfaces';
+import { ConfirmModalComponent } from "../../../shared/component/confirm-modal/confirm-modal.component";
+import { DataTableComponent } from "../../../shared/component/data-table/data-table.component";
 
 @Component({
   selector: 'app-user-admin-page',
-  imports: [],
+  imports: [DataTableComponent],
   templateUrl: './user-admin-page.component.html',
 })
 export class UserAdminPageComponent implements OnInit {
