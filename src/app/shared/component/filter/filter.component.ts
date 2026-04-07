827f2f60fd02ba4af1d1ed88c7108d44a570fb85
JDGB1428 <juangalindo28@hotmail.com>
2026-04-07 12:30:55 -0500

    creando el html para los filtros, haciendo el output para hacer el emit

--- src/app/shared/component/filter/filter.component.ts
+++ src/app/shared/component/filter/filter.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, input, output } from '@angular/core';
 
 @Component({
   selector: 'shared-filter',
@@ -7,5 +7,10 @@ import { Component } from '@angular/core';
 
 })
 export class FilterComponent {
+  activeCategoryId = input.required<number>();
+  categoryChange = output<number>();
 
+  applyFilter(categoryId: number) {
+    this.categoryChange.emit(categoryId);
+  }
 }
