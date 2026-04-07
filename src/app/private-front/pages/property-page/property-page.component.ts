8d41b92fb3b21c66ad75c853e0298feafdb99278
JDGB1428 <juangalindo28@hotmail.com>
2026-04-07 12:31:39 -0500

    refactorizando el codigo, usando rxResource

--- src/app/private-front/pages/property-page/property-page.component.ts
+++ src/app/private-front/pages/property-page/property-page.component.ts
@@ -1,34 +1,67 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, computed, inject, signal } from '@angular/core';
 import { CardPropertyComponent } from "@property/component/card-property/card-property.component";
 import { LoadingCardComponent } from "@shared/component/loading-card/loading-card.component";
 import { Property } from '@interfaces/property.interfaces';
 import { PropertyServices } from '@services/property.service';
+import { rxResource} from '@angular/core/rxjs-interop';
+import { SearchComponent } from "@shared/component/search/search.component";
+import { FilterComponent } from "@shared/component/filter/filter.component";
+import { debounceSignal } from '../../../core/utils/debounce-signal.util';
 
 
 @Component({
   selector: 'app-department-page',
-  imports: [CardPropertyComponent, LoadingCardComponent],
+  imports: [CardPropertyComponent, LoadingCardComponent, SearchComponent, FilterComponent],
   templateUrl: './property-page.component.html',
 })
 export class PropertyPageComponent {
 
-  properties = signal<Property[]>([]);
-  loading = signal<boolean>(true);
-  loadingCards = Array(8);
+  private readonly propertyService = inject(PropertyServices);
 
-  private propertyService = inject(PropertyServices);
+  readonly search = signal('');
+  readonly activeCategoryId = signal(0);
+  readonly loadingCards = Array.from({ length: 8 });
 
-   ngOnInit(): void {
-    setTimeout(() => {
-      this.listProperties();
-      this.loading.set(false);
-    }, 2000);
+  private readonly debouncedSearch = debounceSignal(this.search, 400);
+
+  private readonly criteria = computed(() => ({
+    search: this.debouncedSearch().trim(),
+    categoryId: this.activeCategoryId(),
+  }));
+
+  readonly propertiesResource = rxResource({
+    params: () => this.criteria(),
+    stream: ({ params }) => {
+      const { search, categoryId } = params;
+
+      if (search) {
+        return this.propertyService.searchProperty(search);
+      }
+
+      if (categoryId > 0) {
+        return this.propertyService.filterProperty(categoryId);
+      }
+
+      return this.propertyService.getAllProperty();
+    },
+    defaultValue: {
+      message: '',
+      data: [] as Property[],
+    },
+  });
+
+  readonly properties = computed(() => this.propertiesResource.value().data);
+
+  readonly loading = computed(() => {
+    const status = this.propertiesResource.status();
+    return status === 'loading' || status === 'reloading';
+  });
+
+  onSearchInput(value: string): void {
+    this.search.set(value);
   }
 
-  listProperties(){
-    this.propertyService.getAllProperty().subscribe((properties) => {
-      if(!properties) return [];
-      return this.properties.set(properties.data);
-    })
+  applyFilter(categoryId: number): void {
+    this.activeCategoryId.set(categoryId);
   }
 }
