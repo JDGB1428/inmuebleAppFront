10e193b5cad2fa7863b3c29da03953427933b0f9
JDGB1428 <juangalindo28@hotmail.com>
2026-04-10 09:33:09 -0500

    Chequeado la solicitud ya mandada por el usuario, para que no vuleva manda otra solicitud

--- src/app/private-front/pages/become-owner-page/become-owner-page.component.ts
+++ src/app/private-front/pages/become-owner-page/become-owner-page.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { FormBecomeOwnerComponent } from "../../components/form-become-owner/form-become-owner.component";
+import { RequestServices } from '@services/request.service';
 
 @Component({
   selector: 'app-become-owner-page',
-  imports: [],
+  imports: [FormBecomeOwnerComponent],
   templateUrl: './become-owner-page.component.html',
 })
-export class BecomeOwnerPageComponent {
+export class BecomeOwnerPageComponent implements OnInit {
 
+  private readonly rolRequestService = inject(RequestServices);
+  requestStatus = signal<string|null>(null);
+
+  ngOnInit(): void {
+    this.checkRolRequest();
+  }
+
+  checkRolRequest(){
+    this.rolRequestService.getRoleRequestById().subscribe({
+      next:(res) => {
+        this.requestStatus.set(res.data.status);
+        console.log(res.data.status);
+      },
+      error: () => {
+        this.requestStatus.set(null);
+      }
+    })
+  }
 }
